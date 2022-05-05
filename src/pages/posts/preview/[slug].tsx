fa1498212287da01d86130b9d0b059f01cf65c20
Robson Natanael <natanaelrobson@gmail.com>
2022-05-05 07:25:47 -0300

    feat: generating static views

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -53,7 +53,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
   );
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths = async () => {
   return {
     paths: [],
     fallback: 'blocking'
@@ -81,6 +81,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   return {
     props: {
       post,
-    }
+    },
+    redirect: 60 * 30, // 30 minutos
   }
 }
