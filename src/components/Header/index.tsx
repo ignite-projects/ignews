cbd754df069a9070fce12a88509c5cf8d4595e37
Robson Natanael <natanaelrobson@gmail.com>
2022-06-19 08:49:35 -0300

    refactor: remove obsolete prefetch

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,18 @@
-import { ActiveLink } from '../ActiveLink';
-import { SignInButton } from '../SignInButton';
-import styles from './styles.module.scss';
+import { ActiveLink } from "../ActiveLink";
+import { SignInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
 
 export function Header() {
-
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
           <ActiveLink activeClassName={styles.active} href="/">
             <a>Home</a>
           </ActiveLink>
-          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+          <ActiveLink activeClassName={styles.active} href="/posts">
             <a>Posts</a>
           </ActiveLink>
         </nav>
